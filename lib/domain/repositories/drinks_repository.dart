6b8536b50772bba4c9cd91437a8738681d0d9ca9
Juan José <jujopere@bancolombia.com.co>
2022-12-19 14:51:52 -0500

    se corrige tipo para getDrinkById

--- lib/domain/repositories/drinks_repository.dart
+++ lib/domain/repositories/drinks_repository.dart
@@ -1,7 +1,9 @@
 import 'package:cocktails_app/domain/entities/drink_detail_entity.dart';
 import 'package:cocktails_app/domain/entities/drinks_response_entity.dart';
 
+import '../entities/details_response_entity.dart';
+
 abstract class DrinkRepository {
   Future<DrinkResponseEntity> getDrinks(String endpoint);
-  Future<DrinkDetailsEntity> getDrinkById(String? id);
+  Future<DetailsResponseEntity> getDrinkById(String? id);
 }
